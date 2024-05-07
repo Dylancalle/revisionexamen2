e1bf1b6e08f4552429a52a005410de4eb650c9e6
Dylan Calle <dylan.calle@ucb.edu.bo>
2024-05-07 12:12:38 -0400

    ahora se puede buscar elementos repetidos

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,7 +1,28 @@
-import sumar from "./sumador.js";
-
-describe("Sumar", () => {
-  it("deberia sumar dos numeros", () => {
-    expect(sumar(3, 2)).toEqual(5);
+describe("buscarproeycto", () => {
+  
+  it("Se deberia mostrar todos los nombres de los proyectos buscados en un string", () => {
+    let proyectos = [];
+    proyectos.push("proyecto1")
+    proyectos.push("proyecto2")
+    proyectos.push("proyecto3")
+    proyectos.push("proyecto2")
+    expect(buscarProyecto("proyecto2", proyectos)).toEqual("proyecto2proyecto2")
   });
+ 
 });
+
+function buscarProyecto(nombre, proyectos){
+  let proyectosbuscados = "";
+      if(proyectos.length === 0)
+  {
+      return "";
+  }
+  for(const proyecto of proyectos)
+    {
+      if(proyecto===nombre)
+       {
+       proyectosbuscados=proyectosbuscados+proyecto;
+       }
+    }
+  return proyectosbuscados;
+}
